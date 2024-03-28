886f1038a62a719d83733372e0d595d074262e4c
AKIRO\akizo <akizo.rain@icloud.com>
2024-03-28 22:29:36 +0900

    動画幅の調整

--- components/YoutubeEmbed.tsx
+++ components/YoutubeEmbed.tsx
@@ -1,31 +1,31 @@
 import { Box, Text } from "@chakra-ui/react";
 
 interface YoutubeEmbedProps {
-  embedId: string;
-  title: string;
+    embedId: string;
+    title: string;
 }
 
 const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ embedId, title }) => (
-  <Box>
-    <Text fontSize="lg" fontWeight="bold" mb={4}>
-      {title}
-    </Text>
-    <Box pos="relative" paddingTop="56.25%" w="full" h="0">
-      <iframe
-        src={`https://www.youtube.com/embed/${embedId}`}
-        allowFullScreen
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        title="Embedded youtube"
-        style={{
-          position: "absolute",
-          top: "0",
-          left: "0",
-          width: "100%",
-          height: "100%",
-        }}
-      />
+    <Box w="90%" maxW="560px" mx="auto" mb="5">
+        <Text fontSize="lg" fontWeight="bold" mb={4}>
+            {title}
+        </Text>
+        <Box pos="relative" paddingTop="56.25%" w="full" h="0">
+            <iframe
+                src={`https://www.youtube.com/embed/${embedId}`}
+                allowFullScreen
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                title="Embedded youtube"
+                style={{
+                    position: "absolute",
+                    top: "0",
+                    left: "0",
+                    width: "100%",
+                    height: "100%",
+                }}
+            />
+        </Box>
     </Box>
-  </Box>
 );
 
 export default YoutubeEmbed;
